7c7a4b369002d2b37809141aff5cb8eddd77d3a1
waqas-on-github <waqasvu892@gmail.com>
2024-10-19 11:40:44 +0500

    added countDown

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,44 +1,102 @@
-
-
-import { Text, View, StyleSheet, TouchableOpacity, Alert } from "react-native";
 import { theme } from "../../theme";
-import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync"
+import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
 import * as Notifications from "expo-notifications";
+import { useEffect, useState } from "react";
+import { intervalToDuration, isBefore } from "date-fns";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { TimeSegment } from "../../components/TimeSegment";
+import { getFromStorage, saveToStorage } from "../../utils/storage";
+
+// 10 seconds from now
+const timestamp = Date.now() + 10 * 1000;
+
+const oneDay = 24 * 60 * 60 * 1000;
+const oneHour = 60 * 60 * 1000;
+const oneMinute = 60 * 1000;
+const oneSecond = 1 * 1000;
+
+type CountdownStatus = {
+    isOverdue: boolean;
+    distance: ReturnType<typeof intervalToDuration>;
+};
 
 export default function CounterScreen() {
+    const [status, setStatus] = useState<CountdownStatus>({
+        isOverdue: false,
+        distance: {},
+    });
 
-    const secduleNotification = async () => {
-        const result = await registerForPushNotificationsAsync();
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            const isOverdue = isBefore(timestamp, Date.now());
 
-        if (result === "granted") {
-            const result = await Notifications.scheduleNotificationAsync({
-                content: {
-                    title: "im notification from plantly"
-                },
-                trigger: {
-                    seconds: 5,
-                }
-            })
+            const distance = intervalToDuration(
+                isOverdue
+                    ? { end: Date.now(), start: timestamp }
+                    : { start: Date.now(), end: timestamp }
+            );
 
-            console.log(result);
+            setStatus({ isOverdue, distance });
+        }, 1000);
 
-        } else {
-            Alert.alert(
-                "Unable  to secdule notification",
-                "Enable the notifications permission for Expo Go in settings"
-            )
-        }
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    const scheduleNotification = async () => {
+        await registerForPushNotificationsAsync();
+        await Notifications.scheduleNotificationAsync({
+            content: {
+                title: "Reminder",
+                body: "This is your scheduled notification!",
+            },
+            trigger: { seconds: 2 },
+        });
     };
 
-
     return (
-        <View style={styles.container}>
+        <View
+            style={[
+                styles.container,
+                status.isOverdue ? styles.containerLate : undefined,
+            ]}
+        >
+            {!status.isOverdue ? (
+                <Text style={styles.heading}>Thing due in</Text>
+            ) : (
+                <Text style={[styles.heading, styles.whiteText]}>
+                    Thing overdue by
+                </Text>
+            )}
+            <View style={styles.row}>
+                <TimeSegment
+                    unit="Days"
+                    number={status.distance?.days ?? 0}
+                    textStyle={status.isOverdue ? styles.whiteText : undefined}
+                />
+                <TimeSegment
+                    unit="Hours"
+                    number={status.distance?.hours ?? 0}
+                    textStyle={status.isOverdue ? styles.whiteText : undefined}
+                />
+                <TimeSegment
+                    unit="Minutes"
+                    number={status.distance?.minutes ?? 0}
+                    textStyle={status.isOverdue ? styles.whiteText : undefined}
+                />
+                <TimeSegment
+                    unit="Seconds"
+                    number={status.distance?.seconds ?? 0}
+                    textStyle={status.isOverdue ? styles.whiteText : undefined}
+                />
+            </View>
             <TouchableOpacity
-                onPress={secduleNotification}
+                onPress={scheduleNotification}
                 style={styles.button}
                 activeOpacity={0.8}
             >
-                <Text style={styles.buttonText}>Schedule notification</Text>
+                <Text style={styles.buttonText}>I've done the thing!</Text>
             </TouchableOpacity>
         </View>
     );
@@ -49,18 +107,33 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "#fff",
+        backgroundColor: theme.colors.colorWhite,
     },
     button: {
         backgroundColor: theme.colors.colorBlack,
         padding: 12,
-        borderRadius: 6,
+        borderRadius: 8,
     },
     buttonText: {
-        color: "#fff",
+        color: theme.colors.colorWhite,
         fontWeight: "bold",
         textTransform: "uppercase",
         letterSpacing: 1,
     },
-
+    row: {
+        flexDirection: "row",
+        marginBottom: 24,
+    },
+    heading: {
+        fontSize: 24,
+        fontWeight: "bold",
+        marginBottom: 24,
+        color: theme.colors.colorBlack,
+    },
+    containerLate: {
+        backgroundColor: theme.colors.colorRed,
+    },
+    whiteText: {
+        color: theme.colors.colorWhite,
+    },
 });
